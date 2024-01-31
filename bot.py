a446bf5a6d64a46c5216b0325b2af420a58546c1
kuznecnubas <KuznechikTV@Yandex.kz>
2024-01-31 12:12:44 +0600

    1

--- bot.py
+++ bot.py
@@ -1,12 +1,32 @@
 import telebot
-# Создаем объект, умеющий работать с телеграм API и сразу же передаем ему токен для подключения к боту
 bot = telebot.TeleBot("6437951339:AAHzQBQonTezuyVNJAf6kD9ztMuXrqWJGzA")
 
-@bot.message_handler()
-def echo_all(message):
-    # Команда боту ответить на сообщение, текстом из сообщения
-    bot.reply_to(message, message.text)
-    print(message.text)
+#кнопки
+aboutButton = telebot.types.KeyboardButton("О колледже")
+mainInformationButton = telebot.types.KeyboardButton("Основная информация")
+missionOfColledgeButton = telebot.types.KeyboardButton("Миссия колледжа")
+#клавиатуры
+mainKeyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+mainKeyboard.add(aboutButton)
 
-# Запуск бота, на регулярную отправку запросов новых сообщений с сервера
+aboutKeyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+aboutKeyboard.add(mainInformationButton, missionOfColledgeButton)
+
+
+a =
+
+#хэндлеры
+@bot.message_handler(commands=['start'])
+def startMessage(message):
+    bot.reply_to(message, f"Привет {message.from.first_name}, я информационный бот Костанайского Колледжа Автомобильного Транспорта!", reply_markup=mainKeyboard)
+
+@bot.message_handler()
+def unknownCommandReply(message):
+    if message.text == "О колледже":
+        bot.reply_to(message, "Какую информацию вы хотите получить?", reply_markup=aboutKeyboard)
+    elif message.text == "Основная информация":
+        bot.reply_to(message, "Костанайский колледж автомобильного транспорта (ККАТ) — одно из старейших учебных заведений Костанайской области, обучение ведется по 9 специальностям и 11 квалификациям. Сотрудничество с предприятиями и современная материально-техническая база обеспечивают качественное обучение и хорошее трудоустройство выпускников.", reply_markup=aboutKeyboard)
+    else:
+        bot.reply_to(message, "Я не знаю этой команды")
+
 bot.infinity_polling()
