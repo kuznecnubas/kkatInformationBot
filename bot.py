500f2a014a5861c66f84d4b5a92306be2581c4b1
kuznecnubas <KuznechikTV@Yandex.kz>
2024-01-31 10:29:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import telebot
 # Создаем объект, умеющий работать с телеграм API и сразу же передаем ему токен для подключения к боту
 bot = telebot.TeleBot("6437951339:AAHzQBQonTezuyVNJAf6kD9ztMuXrqWJGzA")
 
@@ -5,6 +6,7 @@ bot = telebot.TeleBot("6437951339:AAHzQBQonTezuyVNJAf6kD9ztMuXrqWJGzA")
 def echo_all(message):
     # Команда боту ответить на сообщение, текстом из сообщения
     bot.reply_to(message, message.text)
+    print(message.text)
 
 # Запуск бота, на регулярную отправку запросов новых сообщений с сервера
 bot.infinity_polling()
